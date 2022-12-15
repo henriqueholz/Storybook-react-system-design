eda3f5665a425450f8b715032884f7fb42073d3c
henriqueholz <henriqueholz@live.com>
2022-12-14 23:05:30 -0300

    Adding more storebook configurations

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/henriqueholz.png',
     alt: 'Henrique Holz',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
